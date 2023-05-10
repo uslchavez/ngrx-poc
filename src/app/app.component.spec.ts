7bb63bda8533402aa6cc25135fa7957a186bf474
uslchavez <luis.chavez@unosquare.com>
2023-05-09 21:31:43 -0400

    wrote unit tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,46 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+import { MatButtonModule } from '@angular/material/button';
+
+import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+fdescribe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, MatButtonModule, MatToolbarModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'poc-ngrx'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('poc-ngrx');
-  });
+  it('should render a toolbar with links', () => {
+    const toolbar: DebugElement = fixture.debugElement.query(
+      By.css('mat-toolbar')
+    );
+    const links: DebugElement[] = fixture.debugElement.queryAll(By.css('a'));
+    const usersAnchor: HTMLAnchorElement = links[0].nativeElement;
+    const authLoginAnchor: HTMLAnchorElement = links[1].nativeElement;
+    const authRegisterAnchor: HTMLAnchorElement = links[2].nativeElement;
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('poc-ngrx app is running!');
+    expect(toolbar).toBeDefined();
+    expect(links.length).toBe(3);
+    expect(usersAnchor.href).toContain('/users');
+    expect(authLoginAnchor.href).toContain('/auth/login');
+    expect(authRegisterAnchor.href).toContain('/auth/register');
   });
 });
