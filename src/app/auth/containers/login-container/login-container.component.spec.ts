3fc360d9937889847da5fbcc590ee735c26e870c
uslchavez <luis.chavez@unosquare.com>
2023-05-14 20:53:52 -0400

    unit tests for login container component

--- src/app/auth/containers/login-container/login-container.component.spec.ts
+++ src/app/auth/containers/login-container/login-container.component.spec.ts
@@ -1,13 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginContainerComponent } from './login-container.component';
-import { StoreModule } from '@ngrx/store';
+import { provideMockStore } from '@ngrx/store/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { UserFormComponent } from '../../components/user-form/user-form.component';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('LoginContainerComponent', () => {
   let component: LoginContainerComponent;
@@ -16,7 +18,6 @@ describe('LoginContainerComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        StoreModule.forRoot({}),
         RouterTestingModule,
         FormsModule,
         MatFormFieldModule,
@@ -24,6 +25,12 @@ describe('LoginContainerComponent', () => {
         BrowserAnimationsModule,
       ],
       declarations: [LoginContainerComponent, UserFormComponent],
+      providers: [
+        provideMockStore({
+          initialState: { auth: { entities: [] } },
+          selectors: [],
+        }),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginContainerComponent);
@@ -33,5 +40,60 @@ describe('LoginContainerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch action after calling onLogin method', () => {
+    const dispatchSpy = spyOn(component['store'], 'dispatch');
+
+    component.onLogin({ email: '', password: '' });
+
+    expect(dispatchSpy).toHaveBeenCalled();
+  });
+
+  it('should dispatch action after calling onLogin method', () => {
+    const dispatchSpy = spyOn(component['store'], 'dispatch');
+
+    component.onRegister({ email: '', password: '' });
+
+    expect(dispatchSpy).toHaveBeenCalled();
+  });
+});
+
+describe('LoginContainerComponent', () => {
+  let component: LoginContainerComponent;
+  let fixture: ComponentFixture<LoginContainerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+      ],
+      declarations: [LoginContainerComponent, UserFormComponent],
+      providers: [
+        provideMockStore({
+          initialState: { auth: { entities: [] } },
+          selectors: [],
+        }),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({
+              page: 'register',
+            }),
+          },
+        },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LoginContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  it('should assign the title depending on the current page', () => {
+    expect(component.title).toEqual('Sign Up Page');
   });
 });
